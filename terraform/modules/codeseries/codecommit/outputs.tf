9d20d815ac7edebeeac59849242b65b060b56bf6
Jeongjewww <gwmelody99@gmail.com>
2023-04-14 16:51:01 +0900

    [ziwooda] feat: codecommit repo resource

---
+++ terraform/modules/codeseries/codecommit/outputs.tf
@@ -0,0 +1,9 @@
+output "repository_name" {
+  value = aws_codecommit_repository.jujy_log_repo.repository_id
+  description = "repository id of codecommit"
+}
+
+output "http_clone_url" {
+  value = aws_codecommit_repository.jujy_log_repo.clone_url_http
+  description = "http url to clone on local"
+}
